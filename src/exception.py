4e5bd5ed646eaac6f43185b8a418962338052e39
Mohammed Abdul Yusuf <mohammedabdulyusuf18@gmal.com>
2025-04-25 20:30:42 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -19,13 +19,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Division by zero error")
-        raise CustomException(e, sys)  
- 
 
 
         
