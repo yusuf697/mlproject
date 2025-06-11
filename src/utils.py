8808e020d43ee237ed08f4b39394343d1fd4bf4e
yusuf697 <mohammedabdulyusuf18@gmail.com>
2025-06-11 12:18:44 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,33 +1,42 @@
 import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-import dill
 
-from sklearn.metrics import r2_score
-
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
-        dir_path =os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        report={}
+        report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -40,6 +49,14 @@ def evaluate_models(X_train, y_train, X_test, y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
